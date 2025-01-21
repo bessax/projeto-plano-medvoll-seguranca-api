2bb5e17a79ab268b902644f580632024357ceefa
bessax <andrebessax@gmail.com>
2025-01-21 11:54:50 -0300

    #vídeo 2.1 - implementação do controller de autenticação e registro de usuários no Identity;

---
+++ MedVoll.Web/Controllers/AuthController.cs
@@ -0,0 +1,50 @@
+﻿using MedVoll.Web.Dtos;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MedVoll.Web.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController:ControllerBase
+{
+    private readonly UserManager<IdentityUser> userManager;
+    private readonly SignInManager<IdentityUser> signInManager;
+
+    public AuthController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+    {
+        this.userManager = userManager;
+        this.signInManager = signInManager;
+    }
+
+    //Endpoints
+    [HttpPost("registrar-usuario")]
+    public async Task<IActionResult> RegistrarUsuarioAsync([FromBody] UsuarioDto usuarioDto)
+    {
+        var usuario = new IdentityUser 
+        {
+            UserName = usuarioDto.Email,
+            Email = usuarioDto.Email, 
+            EmailConfirmed = true 
+        };
+        var result = await userManager.CreateAsync(usuario, usuarioDto.Senha);
+        if (!result.Succeeded)
+        {
+            return BadRequest($"Falha ao registrar usuário : {result.Errors}");
+        }
+        await signInManager.SignInAsync(usuario, isPersistent: false);
+
+        return Ok("Usuário Criado com sucesso!");
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> LoginAsync([FromBody] UsuarioDto usuarioDto)
+    {
+        var result = await signInManager.PasswordSignInAsync(usuarioDto.Email!, usuarioDto.Senha!, isPersistent: false, lockoutOnFailure: false);
+        if (!result.Succeeded)
+        {
+            return BadRequest("Falha no login do usuário.");
+        }
+        return Ok("Logado com sucesso!");
+    }
+}
